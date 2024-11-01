3a33c5cee51b0c9902501170ae177b8b78366abe
Nikodem Bochnacki <nikodem.bochnacki@iaisa.com>
2024-11-01 17:10:03 +0100

    working window, gitignore change

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ def main():
     pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
         surface.fill(000000)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
