6894a0b2c1409465ee5e9f024001fb89bcfa6b22
Nikodem Bochnacki <nikodem.bochnacki@iaisa.com>
2024-11-01 17:53:26 +0100

    added player + objects

--- main.py
+++ main.py
@@ -3,23 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        surface.fill(000000)
+        screen.fill(000000)
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
